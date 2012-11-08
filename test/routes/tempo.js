dc06fc92039d2185b3ce40f84b2bf8700c6f75dc
Cyril Gravelier <magiccyril@gmail.com>
2012-11-08 17:49:42 +0100

    Use should for route test

--- test/routes/tempo.js
+++ test/routes/tempo.js
@@ -1,41 +1,47 @@
 var request  = require('supertest')
   , express  = require('express')
   , assert   = require('assert')
+  , should   = require('should')
   , app      = require('../../app')
   , fixtures = require('../fixtures')
   , mongoose = require('mongoose')
   , Tempo    = require('../../model').Tempo;
 
-describe('POST /tempo', function() {
-    it('respond 412 if no object provided', function(done) {
-        request(app)
-            .post('/tempo')
-            .expect(412, done);
+describe('Tempo API', function() {
+
+  describe('POST /tempo', function() {
+    it('should respond 412 if no object provided', function(done) {
+      request(app)
+        .post('/tempo')
+        .end(function(err, res){
+          res.should.have.status(412);
+          done();
+        });
     });
 
-    it('save object to database and respond 200', function(done) {
-        var object = fixtures.tempo;
+    it('should save object to database and respond 200', function(done) {
+      var object = fixtures.tempo;
 
-        request(app)
-            .post('/tempo')
-            .send({ day: object.day, color: object.color })
-            .expect(200)
-            .end(function(err, res){
-                if (err) {
-                    return done(err);
-                }
+      request(app)
+        .post('/tempo')
+        .send({ day: object.day, color: object.color })
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
 
-                Tempo.findOne({ day: object.day }, function (err, tempo) {
-                    if (err) {
-                        return done(err);
-                    }
+          res.should.have.status(200);
 
-                    if (!tempo) {
-                        throw new Error("Object not found in database");
-                    }
+          Tempo.findOne({ day: object.day }, function (err, tempo) {
+            if (err) {
+              return done(err);
+            }
 
-                    done();
-                });
-            });
+            should.exist(tempo);
+
+            done();
+          });
+        });
     });
+  });
 });
